1e2e887e0b40784437c46029c3b15b21f25c2651
koksoybedirhan <71734713+koksoybedirhan@users.noreply.github.com>
2022-02-25 14:47:00 +0300

    Add files via upload

---
+++ File/StepMotor_Rastgele.ino
@@ -0,0 +1,29 @@
+#include <Arduino.h>
+#include <time.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#define rastgeleMax 2
+#define rastgeleMin 1
+
+String value = ",";
+
+void setup() {
+    Serial.begin(9600);
+    pinMode(13, OUTPUT);
+}
+
+void loop() {
+    srand(time(0));
+    long a;
+
+    for (int i = 1; i > 0; ++i) {
+        a = rand() % rastgeleMax + rastgeleMin;
+
+        Serial.print(a + value);
+        pinMode(13, HIGH);
+        delay(1000);
+        pinMode(13, LOW);
+        delay(1000);
+    }
+}
